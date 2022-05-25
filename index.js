cf5d01718824060ee3e4625eb2daeb181bf0f2ac
Brenda309 <brewihogora23@gmail.com>
2022-05-25 22:57:24 +0200

    Add the remove function.

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
-//DOM elements
-const bookList= document.getElementById("book-list")
-const form = document.getElementById("book-form");
-const addBtn= document.getElementById("btn");
-
-let bookData ={
-    title: "",
-    author: ""
-
-}
-let books= JSON.parse(localStorage.getItem("books"))|| [];
-// preserved data in the browser's memory by using localStorage.
-addBtn.addEventListener ('click', function() {
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-if(!titleInput.value || !authorInput.value){
-    alert ("Please add book title and book author")
-}
-else {
-    const newBook= Object.create(bookData);
-    newBook.title = titleInput.value;
-    newBook.author = authorInput.value;
-    books.push(newBook);
-    titleInput.value ="";
-    authorInput.value="";
-    localStorage.setItem("books", JSON.stringify(books));
-    window.location.reload();
+// Book Object
+const Book = {
+  title: '',
+  author: '',
 };
-})
-if(localStorage.books){
-for( let i = 0; JSON.parse(localStorage.books).length; i += 1){
-    const bookDiv = document.createElement("div");
-    bookDiv.id= `${i}`;
-    const bookTitle= document.createElement("h3");
-    const bookAuthor = document.createElement("h3");
-    const removeBtn= document.createElement("button")
-    removeBtn.className = "rmv";
-    const horizontalLine= document.createElement("hr")
+// Books Collection
+let Books = [];
+if (localStorage.Books) {
+  Books = JSON.parse(localStorage.Books);
+}
 
-    bookTitle.innerText = "Book title:" + title;
-    bookAuthor.innerText = "Author name:" + author;
-    removeBtn.innerText= "remove"
-bookDiv.append(bookTitle,bookAuthor, removeBtn,horizontalLine);
-bookList.appendChild(bookDiv);
+// populate local storage collection with user entries
+const addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', () => {
+  const bookTitle = document.getElementById('title');
+  const author = document.getElementById('author');
+  if (!bookTitle.value || !author.value) {
+    alert('Enter both book title and author please');
+  } else {
+    const book = Object.create(Book);
+    book.title = bookTitle.value;
+    book.author = author.value;
+    Books.push(book);
+    bookTitle.value = '';
+    author.value = '';
+    const stringData = JSON.stringify(Books);
+    localStorage.setItem('Books', stringData);
+    window.location.reload();
+  }
+});
+
+// Display localStorage Books Collection entries
+if (localStorage.Books) {
+  for (let i = 0; i < JSON.parse(localStorage.Books).length; i += 1) {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.id = `${i}`;
+    li.style.listStyle = 'none';
+    const addTitle = document.createElement('p');
+    addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
+    li.appendChild(addTitle);
+    const addAuthor = document.createElement('p');
+    addAuthor.innerHTML = JSON.parse(localStorage.Books)[i].author;
+    li.appendChild(addAuthor);
+    const rmvButton = document.createElement('button');
+    rmvButton.innerHTML = 'Remove';
+    rmvButton.id = `btn${i}`;
+    rmvButton.className = 'rmv';
+    li.appendChild(rmvButton);
+    const hr = document.createElement('hr');
+    li.appendChild(hr);
+    ul.appendChild(li);
+  }
 }
+
+// Remove books from collection and display
+for (let i = 0; i < document.getElementsByClassName('rmv').length; i += 1) {
+  const rmvButton = document.getElementById(`btn${i}`);
+  const titleName = Books[i].title;
+  const list = document.getElementById(`${i}`);
+  rmvButton.addEventListener('click', () => {
+    const filtered = Books.filter((Books) => Books.title !== titleName);
+    const stringData = JSON.stringify(filtered);
+    localStorage.setItem('Books', stringData);
+    list.remove();
+    window.location.reload();
+  });
 }
-//a function to display a new book to the collection 
