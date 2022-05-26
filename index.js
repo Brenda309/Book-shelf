5a30b86d200ec47297f3ea32aede76b6948f5522
Brenda309 <brewihogora23@gmail.com>
2022-05-26 19:18:01 +0200

    Fix linter error

--- index.js
+++ index.js
@@ -1,67 +1,65 @@
-class Books {
-    constructor(array){
-        this.array = []
-    }
-}
-
-class Book {
-    constructor(title,author){
-        this.title = title;
-        this.author = author;
-    }
-}
-const storage = new Books()
-if (localStorage.Books){
-    storage.array = JSON.parse(localStorage.Books)
-}
-const addBtn = document.getElementById('addBtn');
-addBtn.addEventListener ('click', ()=> {
-    const booksTitle = document.getElementById('title');
-    const author = document.getElementById('author');
-    if(!booksTitle.value || !author.value){
-        alert('Enter both title books and author');
-    } else{
-        const book = new Book(booksTitle.value, author.value);
-        storage.array.push(book);
-        book.value ='';
-        author.value ='';
-        const stringData = JSON.stringify(storage.array)
-        localStorage. setItem('Books', stringData);
-        window.location.reload();
-    };
-});
-
-
-if(localStorage.Books){
-    for (let i = 0; i < JSON.parse(localStorage.Books).length; i += 1){
-const addTitle = document.createElement('p');
-const ul = document.querySelector('ul');
-const li = document.createElement('li');
-const addAuthor = document.createElement('p')
-const rmvButton = document.createElement('button');
-
-
-li.id = `${i}`;
-li.style.listStyle ='none';
-
-addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
-addTitle.innerText += ' by ';
-li.appendChild(addTitle);
-
-addAuthor.innerHTML =JSON.parse(localStorage.Books)[i].author;
-li.appendChild(addAuthor);
-
-rmvButton.innerHTML = 'Remove';
-rmvButton.id = `btn${i}`;
-rmvButton.className = 'rmv';
-li.appendChild(rmvButton);
-
-
-ul.appendChild(li);
+class Books {/* eslint-disable-line max-classes-per-file */
+  constructor() {
+    this.array = [];
   }
 }
 
-for (let i = 0; i < document.getElementsByClassName('rmv').length; i += 1) {
+class Book {/* eslint-disable-line max-classes-per-file */
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+const booksTitle = document.getElementById('title');
+const author = document.getElementById('author');
+const addBtn = document.getElementById('addBtn');
+const storage = new Books();
+
+if (localStorage.Books) {
+  storage.array = JSON.parse(localStorage.Books);
+}
+addBtn.addEventListener('click', () => {
+  if (!booksTitle.value || !author.value) {
+    alert('Enter both title books and author');
+  } else {
+    const book = new Book(booksTitle.value, author.value);
+    storage.array.push(book);
+    book.value = '';
+    author.value = '';
+    const stringData = JSON.stringify(storage.array);
+    localStorage.setItem('Books', stringData);
+    window.location.reload();
+  }
+});
+
+if (localStorage.Books) {
+  for (let i = 0; i < JSON.parse(localStorage.Books).length; i += 1) {
+    const addTitle = document.createElement('p');
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    const addAuthor = document.createElement('p');
+    const rmvButton = document.createElement('button');
+
+    li.id = `${i}`;
+    li.style.listStyle = 'none';
+
+    addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
+    addTitle.innerText += ' by ';
+    li.appendChild(addTitle);
+
+    addAuthor.innerHTML = JSON.parse(localStorage.Books)[i].author;
+    li.appendChild(addAuthor);
+
+    rmvButton.innerHTML = 'Remove';
+    rmvButton.id = `btn${i}`;
+    rmvButton.className = 'rmv';
+    li.appendChild(rmvButton);
+
+    ul.appendChild(li);
+  }
+}
+const rmv = document.getElementsByClassName('rmv');
+for (let i = 0; i < rmv.length; i += 1) {
   const rmvButton = document.getElementById(`btn${i}`);
   const titleName = storage.array[i].title;
   const list = document.getElementById(`${i}`);
