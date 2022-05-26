a43d75b813764e67b79e5e5c7e3cc9e1f90c1e47
Brenda309 <brewihogora23@gmail.com>
2022-05-26 18:06:30 +0200

    Add the add book function

--- index.js
+++ index.js
@@ -1,75 +1,64 @@
-class Books { /* eslint-disable-line max-classes-per-file */
-  constructor(array) { /* eslint-disable-line no-unused-vars */
-    this.array = [];
-  }
+class Books {
+    constructor(array){
+        this.array = []
+    }
 }
 
-class Book { /* eslint-disable-line max-classes-per-file */
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
+class Book {
+    constructor(title,author){
+        this.title = title;
+        this.author = author;
+    }
 }
-const storage = new Books();
-
-if (localStorage.Books) {
-  Books = JSON.parse(localStorage.Books);
+const storage = new Books()
+if (localStorage.Books){
+    storage.array = JSON.parse(localStorage.Books)
 }
-
-// populate local storage collection with user entries
 const addBtn = document.getElementById('addBtn');
-addBtn.addEventListener('click', () => {
-  const bookTitle = document.getElementById('title');
-  const author = document.getElementById('author');
-  if (!bookTitle.value || !author.value) {
-    alert('Enter both book title and author please');
-  } else {
-    const book = Object.create(Book);
-    book.title = bookTitle.value;
-    book.author = author.value;
-    Books.push(book);
-    bookTitle.value = '';
-    author.value = '';
-    const stringData = JSON.stringify(Books);
-    localStorage.setItem('Books', stringData);
-    window.location.reload();
-  }
+addBtn.addEventListener ('click', ()=> {
+    const booksTitle = document.getElementById('title');
+    const author = document.getElementById('author');
+    if(!booksTitle.value || !author.value){
+        alert('Enter both title books and author');
+    } else{
+        const book = new Book(booksTitle.value, author.value);
+        storage.array.push(book);
+        book.value ='';
+        author.value ='';
+        const stringData = JSON.stringify(storage.array)
+        localStorage. setItem('Books', stringData);
+        window.location.reload();
+    };
 });
 
-// Display localStorage Books Collection entries
-if (localStorage.Books) {
-  for (let i = 0; i < JSON.parse(localStorage.Books).length; i += 1) {
-    const ul = document.querySelector('ul');
-    const li = document.createElement('li');
-    li.id = `${i}`;
-    li.style.listStyle = 'none';
-    const addTitle = document.createElement('p');
-    addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
-    li.appendChild(addTitle);
-    const addAuthor = document.createElement('p');
-    addAuthor.innerHTML = JSON.parse(localStorage.Books)[i].author;
-    li.appendChild(addAuthor);
-    const rmvButton = document.createElement('button');
-    rmvButton.innerHTML = 'Remove';
-    rmvButton.id = `btn${i}`;
-    rmvButton.className = 'rmv';
-    li.appendChild(rmvButton);
-    const hr = document.createElement('hr');
-    li.appendChild(hr);
-    ul.appendChild(li);
+
+if(localStorage.Books){
+    for (let i = 0; i < JSON.parse(localStorage.Books).length; i += 1){
+const addTitle = document.createElement('p');
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+const addAuthor = document.createElement('p')
+const rmvButton = document.createElement('button');
+const hr = document.createElement('hr');
+
+li.id = `${i}`;
+li.style.listStyle ='none';
+
+addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
+addTitle.innerText += ' by ';
+li.appendChild(addTitle);
+
+addAuthor.innerHTML =JSON.parse(localStorage.Books)[i].author;
+li.appendChild(addAuthor);
+
+rmvButton.innerHTML = 'Remove';
+rmvButton.id = `btn${i}`;
+rmvButton.className = 'rmv';
+li.appendChild(rmvButton);
+
+
+li.appendChild(hr);
+ul.appendChild(li);
   }
 }
 
-// Remove books from collection and display
-for (let i = 0; i < document.getElementsByClassName('rmv').length; i += 1) {
-  const rmvButton = document.getElementById(`btn${i}`);
-  const titleName = Books[i].title;
-  const list = document.getElementById(`${i}`);
-  rmvButton.addEventListener('click', () => {
-    const filtered = Books.filter((Books) => Books.title !== titleName);
-    const stringData = JSON.stringify(filtered);
-    localStorage.setItem('Books', stringData);
-    list.remove();
-    window.location.reload();
-  });
-}
