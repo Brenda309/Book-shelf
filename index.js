52e8b4291918e4f340d2a677cb2b6092828eee02
Brenda309 <brewihogora23@gmail.com>
2022-05-26 22:07:17 +0200

    Fix linter error.

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const author = document.getElementById('author');
 const addBtn = document.getElementById('addBtn');
 const storage = new Books();
 
-
 if (localStorage.Books) {
   storage.array = JSON.parse(localStorage.Books);
 }
@@ -34,51 +33,49 @@ addBtn.addEventListener('click', () => {
   }
 });
 
+const add = () => {
+  if (localStorage.Books) {
+    for (let i = 0; i < JSON.parse(localStorage.Books).length; i += 1) {
+      const addTitle = document.createElement('p');
+      const ul = document.querySelector('ul');
+      const li = document.createElement('li');
+      const addAuthor = document.createElement('p');
+      const rmvButton = document.createElement('button');
 
- const add = () => {
-if (localStorage.Books) {
-  for (let i = 0; i < JSON.parse(localStorage.Books).length; i += 1) {
-    const addTitle = document.createElement('p');
-    const ul = document.querySelector('ul');
-    const li = document.createElement('li');
-    const addAuthor = document.createElement('p');
-    const rmvButton = document.createElement('button');
+      li.id = `${i}`;
+      li.style.listStyle = 'none';
 
-    li.id = `${i}`;
-    li.style.listStyle = 'none';
+      addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
+      addTitle.innerText += ' by ';
+      li.appendChild(addTitle);
 
-    addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
-    addTitle.innerText += ' by ';
-    li.appendChild(addTitle);
+      addAuthor.innerHTML = JSON.parse(localStorage.Books)[i].author;
+      li.appendChild(addAuthor);
 
-    addAuthor.innerHTML = JSON.parse(localStorage.Books)[i].author;
-    li.appendChild(addAuthor);
+      rmvButton.innerHTML = 'Remove';
+      rmvButton.id = `btn${i}`;
+      rmvButton.className = 'rmv';
+      li.appendChild(rmvButton);
 
-    rmvButton.innerHTML = 'Remove';
-    rmvButton.id = `btn${i}`;
-    rmvButton.className = 'rmv';
-    li.appendChild(rmvButton);
-
-    ul.appendChild(li);
+      ul.appendChild(li);
+    }
   }
-}
-}
+};
 add();
 
-
-const remove= () =>{
-const rmv = document.getElementsByClassName('rmv');
-for (let i = 0; i < rmv.length; i += 1) {
-  const rmvButton = document.getElementById(`btn${i}`);
-  const titleName = storage.array[i].title;
-  const list = document.getElementById(`${i}`);
-  rmvButton.addEventListener('click', () => {
-    const filtered = storage.array.filter((Books) => Books.title !== titleName);
-    const stringData = JSON.stringify(filtered);
-    localStorage.setItem('Books', stringData);
-    list.remove();
-    window.location.reload();
-  });
-}
-}
+const remove = () => {
+  const rmv = document.getElementsByClassName('rmv');
+  for (let i = 0; i < rmv.length; i += 1) {
+    const rmvButton = document.getElementById(`btn${i}`);
+    const titleName = storage.array[i].title;
+    const list = document.getElementById(`${i}`);
+    rmvButton.addEventListener('click', () => {
+      const filtered = storage.array.filter((Books) => Books.title !== titleName);
+      const stringData = JSON.stringify(filtered);
+      localStorage.setItem('Books', stringData);
+      list.remove();
+      window.location.reload();
+    });
+  }
+};
 remove();
